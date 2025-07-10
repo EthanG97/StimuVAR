3228d1fac6dfad10542840426aa034ac71931526
EthanG97 <96099572+EthanG97@users.noreply.github.com>
2025-07-09 21:05:52 -0400

    Update tokenizer.py

--- dataset/tokenizer.py
+++ dataset/tokenizer.py
@@ -121,6 +121,89 @@ def preprocess(
         labels=targets,
     )
 
+def preprocess_demo(
+    sources,
+    tokenizer: PreTrainedTokenizer,
+    conv_mode: str = "llama",
+    num_patch_tokens: int = 0,
+    num_frame_tokens: int = 1,
+    use_im_start_end: bool = False,
+    test: bool = False,
+    **kwargs,
+) -> Dict:
+    conv = conversation_lib.conv_templates[conv_mode].copy()
+    roles = {"user": conv.roles[0], "gpt": conv.roles[1]}
+
+    # Apply prompt templates
+    conversations = []
+    for i, source in enumerate(sources):
+        if roles[source[0]["from"]] != conv.roles[0]:
+            # Skip the first one if it is not from human
+            source = source[1:]
+
+        conv.messages = []
+        for j, sentence in enumerate(source):
+            role = roles[sentence["from"]]
+            assert role == conv.roles[j % 2], f"{i}"
+            if use_im_start_end and DEFAULT_IMAGE_TOKEN in sentence["value"]:
+                sentence["value"] = sentence["value"].replace(DEFAULT_IMAGE_TOKEN, 
+                                                              DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN)
+            sentence["value"] = sentence["value"].replace(DEFAULT_IMAGE_TOKEN, 
+                                                            DEFAULT_IMAGE_TOKEN * num_frame_tokens + DEFAULT_IM_PATCH_TOKEN * num_patch_tokens)
+            conv.append_message(role, sentence["value"])
+        conversations.append(conv.get_prompt())
+
+    conversation = conversations[0]
+
+    # Tokenize conversations
+    input_ids = tokenizer(
+        conversations,
+        return_tensors="pt",
+        padding="longest",
+        max_length=2048,
+        truncation=True,
+    ).input_ids
+
+    targets = input_ids.clone()
+
+    # Mask targets
+    for conversation, target in zip(conversations, targets):
+        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+
+        rounds = conversation.split(conv.sep2)
+
+        cur_len = 1
+        target[:cur_len] = IGNORE_INDEX
+        for i, rou in enumerate(rounds):
+            if rou == "":
+                break
+
+            parts = rou.split(conv.sep)
+            if len(parts) != 2:
+                break
+            parts[0] += conv.sep
+
+            round_len = len(tokenizer(rou).input_ids)  
+            instruction_len = len(tokenizer(parts[0]).input_ids) - 2 
+
+            target[cur_len : cur_len + instruction_len] = IGNORE_INDEX
+
+            cur_len += round_len
+        target[cur_len:] = IGNORE_INDEX
+        
+        if not test:
+            if cur_len < tokenizer.model_max_length:
+                if cur_len != total_len:
+                    target[:] = IGNORE_INDEX
+                    print(
+                        f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                        f" (ignored)"
+                    )
+
+    return dict(
+        input_ids=input_ids,
+        labels=targets,
+    )
 
 def prompt(
     sources,
